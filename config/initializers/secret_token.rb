b893f914e96d8d5ec50eb2e08c0b9da65d49d062
Brandt Lareau <newdark1@gmail.com>
2012-08-14 12:49:15 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorworkout::Application.config.secret_token = 'b7315ced1992dbe5eedfca1ed727e782fdbe8088286e34bf570e6c6b6c1e4b53170269240fdd37c7f44d47b5cdb62d47cb1e9541e020bf4990ff76b61347f3fa'
