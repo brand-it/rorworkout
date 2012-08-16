3ec32f7ad9ccb9cdb4112d227005e0e640801176
Brandt Lareau <newdark1@gmail.com>
2012-08-15 20:12:02 -0700

    fixed a syntax error

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :doctor_id, :user_id
-  belongs_to :created_by, :foreign_key => :created_by
+  belongs_to :created_by_user, :foreign_key => :created_by, :class_name => User
   
   validates_presence_of :first_name
   validates_presence_of :last_name
